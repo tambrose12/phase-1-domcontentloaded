85d6770158482e4f72319f1ff8ba3ee137d5ff65
Taylor Ambrose <taykesby@gmail.com>
2023-02-01 09:37:19 -0500

    lab finished.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+function changeParagraph() {
+    const theParagraph = document.getElementById('text')
+    theParagraph.textContent = "This is really cool!"
+}
+
+
+document.addEventListener('DOMContentLoaded', changeParagraph);
